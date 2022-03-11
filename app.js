3731a74fff9322a3b79e47f07cb157ef32bfd252
chris_chan <chrischandfw@gmail.com>
2022-03-10 22:03:15 -0600

    added styling to css, completed html file, and grabbed dom elements on app js file

--- app.js
+++ app.js
@@ -1,8 +1,33 @@
 // import functions and grab DOM elements
+const cityNameInput = document.getElementById('city-name-input');
+const cityNameEl = document.getElementById('city-name');
+const waterDropdown = document.getElementById('water-dropdown');
+const skylineDropdown = document.getElementById('skyline-dropdown');
+const natureDropdown = document.getElementById('nature-dropdown');
+const countMessageEl = document.getElementById('count-message');
+const waterImgEl = document.getElementById('water-img');
+const skylineImgEl = document.getElementById('skyline-img');
+const natureImgEl = document.getElementById('nature-img');
+const addSloganButton = document.getElementById('add-slogan-button');
+const sloganInputEl = document.getElementById('slogan-input');
 
 // let state
+let waterCount = 0;
+let skylineCount = 0;
+let natureCount = 0;
+const slogans = [];
 
 // set event listeners 
+addSloganButton.addEventListener('click', () =>) {
+	const newSlogan = sloganInputEl.value;
+
+	slogans.push(newSlogan);
+
+	displaySlogans();
+
+	sloganInputEl.value ='';
+});
+
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
