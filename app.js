bb926004b49eeed7b31a8a0b51ba899fa94aed50
chris_chan <chrischandfw@gmail.com>
2022-03-10 22:31:23 -0600

    added function and event listener for slogan button

--- app.js
+++ app.js
@@ -8,8 +8,10 @@ const countMessageEl = document.getElementById('count-message');
 const waterImgEl = document.getElementById('water-img');
 const skylineImgEl = document.getElementById('skyline-img');
 const natureImgEl = document.getElementById('nature-img');
-const addSloganButton = document.getElementById('add-slogan-button');
+const addSloganButton = document.getElementById('slogan-button');
 const sloganInputEl = document.getElementById('slogan-input');
+
+
 
 // let state
 let waterCount = 0;
@@ -18,7 +20,7 @@ let natureCount = 0;
 const slogans = [];
 
 // set event listeners 
-addSloganButton.addEventListener('click', () =>) {
+addSloganButton.addEventListener('click', () =>); {
 	const newSlogan = sloganInputEl.value;
 
 	slogans.push(newSlogan);
@@ -26,8 +28,23 @@ addSloganButton.addEventListener('click', () =>) {
 	displaySlogans();
 
 	sloganInputEl.value ='';
-});
+};
 
+function displaySlogans() {
+    // now that we have updated our slogans array . 
+    const listEl = document.getElementById('list');
+
+    listEl.textContent = '';
+  
+    for (let slogan of slogans) {
+        const div = document.createElement('div');
+  
+        div.classList.add('slogan');
+        div.textContent = slogan;
+    // append this NEW element to an OLD element
+        listEl.append(div);
+    }  
+}
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
