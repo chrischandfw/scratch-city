5fdd66651b763e6eebb2c3b98dfbd53ba2ae25cf
chris_chan <chrischandfw@gmail.com>
2022-03-11 13:43:16 -0600

    added event listener for city name input in app js

--- app.js
+++ app.js
@@ -1,50 +1,55 @@
 // import functions and grab DOM elements
 const cityNameInput = document.getElementById('city-name-input');
 const cityNameEl = document.getElementById('city-name');
+
 const waterDropdown = document.getElementById('water-dropdown');
 const skylineDropdown = document.getElementById('skyline-dropdown');
 const natureDropdown = document.getElementById('nature-dropdown');
+
 const countMessageEl = document.getElementById('count-message');
+
 const waterImgEl = document.getElementById('water-img');
 const skylineImgEl = document.getElementById('skyline-img');
 const natureImgEl = document.getElementById('nature-img');
+
 const addSloganButton = document.getElementById('slogan-button');
 const sloganInputEl = document.getElementById('slogan-input');
 
 
-
-// let state
 let waterCount = 0;
 let skylineCount = 0;
 let natureCount = 0;
 const slogans = [];
 
-// set event listeners 
-addSloganButton.addEventListener('click', () =>); {
-	const newSlogan = sloganInputEl.value;
+addSloganButton.addEventListener('click', () => {
+    const newSlogan = sloganInputEl.value;
 
-	slogans.push(newSlogan);
+    slogans.push(newSlogan);
 
-	displaySlogans();
+    displaySlogans();
 
-	sloganInputEl.value ='';
-};
+    sloganInputEl.value = '';
+});
 
 function displaySlogans() {
-    // now that we have updated our slogans array . 
     const listEl = document.getElementById('list');
 
     listEl.textContent = '';
   
     for (let slogan of slogans) {
-        const div = document.createElement('div');
+        const pTag = document.createElement('p');
   
-        div.classList.add('slogan');
-        div.textContent = slogan;
-    // append this NEW element to an OLD element
-        listEl.append(div);
+        pTag.classList.add('slogan');
+        pTag.textContent = slogan;
+        listEl.append(pTag);
     }  
 }
+
+cityNameInput.addEventListener('input', () => {
+    cityNameEl.textContent = cityNameInput.value;
+});
+
+
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
